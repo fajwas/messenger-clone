20715f7291b91ad123e9e6453d1a51d7776da1d3
fajwas <fajwas5433@gmail.com>
2024-02-17 17:03:53 +0530

    first push

---
+++ app/layout.tsx
@@ -0,0 +1,31 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+
+import ToasterContext from "./context/ToasterContext";
+
+import "./globals.css";
+import AuthContext from "./context/AuthContext";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "messenger clone",
+  description: " messenger clone",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>
+        <AuthContext>
+        < ToasterContext />
+          {children}
+        </AuthContext>
+      </body>
+    </html>
+  );
+}
